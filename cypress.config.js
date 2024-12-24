3e848f6ead7ec99105850487859e52ed545328a6
Gastón Hortas <gaston.hortas@globallogic.com>
2024-12-23 21:32:43 -0300

    Fixing CI

--- cypress.config.js
+++ cypress.config.js
@@ -6,16 +6,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       config.env.BASE_URL = process.env.BASE_URL
       config.env.CYPRESS_ENVIRONMENT = process.env.CYPRESS_ENVIRONMENT
-      require('@shelex/cypress-allure-plugin/writer')(on, config);
       return config;
     },
     video: true,
-    videoUploadOnPasses: true,
     screenshots: true,
     trashAssetsBeforeRuns: false,
-    reporter: '@shelex/cypress-allure-plugin',
-    reporterOptions: {
-      debug: true,
-    }
   },
 });
