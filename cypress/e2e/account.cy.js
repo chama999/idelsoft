508f9682c0ad4fb1867338d67a4fa9fdf058d6fc
Gastón Hortas <gaston.hortas@globallogic.com>
2024-12-26 12:18:41 -0300

    Adding update profile test

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -1,9 +1,15 @@
-//utilizando createNewAccountCommand este comando necesito generar una precondition de un describe para que se genere una cuenta antes de cada test
-
+import AccountPage from '../support/pages/accountPage'
+const accountPage = new AccountPage()
 describe('Account Tests', () => {
     beforeEach('Pre-condition', () => {
         cy.createNewAccountCommand()
     })
     it('Update Profile', () => {
+        cy.get(accountPage.locators.accountSubTitle).should('contain', 'Account Information')
+        cy.get(accountPage.locators.editLink).first().click()
+        cy.get(accountPage.locators.firstNameInput).clear().type('John')
+        cy.get(accountPage.locators.lastNameInput).clear().type('Doe')
+        cy.get(accountPage.locators.saveButton).click()
+        cy.get(accountPage.locators.successMessageArea).should('contain', 'You saved the account information.')
     })
 })
--- cypress/support/pages/accountPage.js
+++ cypress/support/pages/accountPage.js
@@ -1,5 +1,10 @@
 export default class AccountPage {
     locators={
+        accountSubTitle:'div > strong',
+        editLink:'div.block-content > div > div.box-actions > a.action.edit > span',
+        firstNameInput:'input[name="firstname"]',
+        lastNameInput:'input[name="lastname"]',
+        saveButton:'button[title="Save"]',
         successMessageArea:'div[role="alert"]'
     }
 }
